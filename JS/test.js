4e3a2d1669a1568dc4a54d35ffa56de820b61fac
DeltY97 <deldelta97@gmail.com>
2024-11-19 15:29:25 +0100

    JavaScript

---
+++ JS/test.js
@@ -0,0 +1,13 @@
+//console.log("Hello World!");
+
+//let UserName = "DeltY";     //string (chaîne de caractères)
+//let Age = 20;               //int (nombre entier)
+//let Size = 1.77;            //float (nombre à virgule)
+//let IsCool = false;         //bool (variable en true/false)
+//console.log(Size-0.02)
+
+let a = 34
+let b = 12
+
+console.log(a+b, a/b, a-b, a*b, a%b)
+
